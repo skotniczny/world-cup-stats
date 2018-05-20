8cde4fe0f41775c3ba6d5a10b24105289c3d9b7e
Lukasz <skotniczny@outlook.com>
2018-05-20 20:44:07 +0200

    Create variables the let and const keywords

--- charts/scripts.js
+++ charts/scripts.js
@@ -4,25 +4,25 @@ Chart.defaults.global.maintainAspectRatio = true
 Chart.defaults.global.layout.padding.top = 20
 
 Chart.plugins.register({
-  afterDatasetsDraw: function (chart) {
-    var ctx = chart.ctx
-    chart.data.datasets.forEach(function (dataset, i) {
-      var meta = chart.getDatasetMeta(i)
+  afterDatasetsDraw: (chart) => {
+    const ctx = chart.ctx
+    chart.data.datasets.forEach((dataset, i) => {
+      const meta = chart.getDatasetMeta(i)
       if (!meta.hidden) {
         meta.data.forEach((element, index) => {
           // Draw the text in black, with the specified font
           ctx.fillStyle = 'rgb(0, 0, 0)'
-          var fontSize = 16
-          var fontStyle = 'normal'
-          var fontFamily = 'Segoe UI, Helvetica Neue, Arial, sans-serif'
+          const fontSize = 16
+          const fontStyle = 'normal'
+          const fontFamily = 'Segoe UI, Helvetica Neue, Arial, sans-serif'
           ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily)
           // Just naively convert to string for now
-          var dataString = dataset.data[index].toString()
+          const dataString = dataset.data[index].toString()
           // Make sure alignment settings are correct
           ctx.textAlign = 'center'
           ctx.textBaseline = 'middle'
-          var padding = 5
-          var position = element.tooltipPosition()
+          const padding = 5
+          const position = element.tooltipPosition()
           ctx.fillText(dataString, position.x, position.y - (fontSize / 2) - padding)
         })
       }
@@ -30,44 +30,44 @@ Chart.plugins.register({
   }
 })
 
-var ctxBrazil = document.getElementById('brazil').getContext('2d')
+const ctxBrazil = document.getElementById('brazil').getContext('2d')
 // eslint-disable-next-line no-unused-vars
-var brazilChart = new Chart(ctxBrazil, {
+const brazilChart = new Chart(ctxBrazil, {
   type: 'bar',
   data: makeData(DATA.brazil.mostFrequentScores)
 })
 
-var ctxSouthAfrica = document.getElementById('south_africa').getContext('2d')
+const ctxSouthAfrica = document.getElementById('south_africa').getContext('2d')
 // eslint-disable-next-line no-unused-vars
-var southAfricaChart = new Chart(ctxSouthAfrica, {
+const southAfricaChart = new Chart(ctxSouthAfrica, {
   type: 'bar',
   data: makeData(DATA.south_africa.mostFrequentScores)
 })
 
-var ctxGermany = document.getElementById('germany').getContext('2d')
+const ctxGermany = document.getElementById('germany').getContext('2d')
 // eslint-disable-next-line no-unused-vars
-var germanyChart = new Chart(ctxGermany, {
+const germanyChart = new Chart(ctxGermany, {
   type: 'bar',
   data: makeData(DATA.germany.mostFrequentScores)
 })
 
-var ctxKoreaJapan = document.getElementById('korea_japan').getContext('2d')
+const ctxKoreaJapan = document.getElementById('korea_japan').getContext('2d')
 // eslint-disable-next-line no-unused-vars
-var koreaJapanChart = new Chart(ctxKoreaJapan, {
+const koreaJapanChart = new Chart(ctxKoreaJapan, {
   type: 'bar',
   data: makeData(DATA.korea_japan.mostFrequentScores)
 })
 
-var ctxFrance = document.getElementById('france').getContext('2d')
+const ctxFrance = document.getElementById('france').getContext('2d')
 // eslint-disable-next-line no-unused-vars
-var franceChart = new Chart(ctxFrance, {
+const franceChart = new Chart(ctxFrance, {
   type: 'bar',
   data: makeData(DATA.france.mostFrequentScores)
 })
 
-var ctxLast5 = document.getElementById('last_5').getContext('2d')
+const ctxLast5 = document.getElementById('last_5').getContext('2d')
 // eslint-disable-next-line no-unused-vars
-var last5Chart = new Chart(ctxLast5, {
+const last5Chart = new Chart(ctxLast5, {
   type: 'bar',
   data: makeData(DATA.last_5_world_cups.mostFrequentScores),
   options: {
@@ -78,9 +78,9 @@ var last5Chart = new Chart(ctxLast5, {
   }
 })
 
-var ctxLast5Group = document.getElementById('last_5_group').getContext('2d')
+const ctxLast5Group = document.getElementById('last_5_group').getContext('2d')
 // eslint-disable-next-line no-unused-vars
-var last5GroupChart = new Chart(ctxLast5Group, {
+const last5GroupChart = new Chart(ctxLast5Group, {
   type: 'bar',
   data: makeData(DATA.last_5_world_cups.mostFrequentScoresGroupStage),
   options: {
@@ -91,9 +91,9 @@ var last5GroupChart = new Chart(ctxLast5Group, {
   }
 })
 
-var ctxLast5PlayOff = document.getElementById('last_5_playoff').getContext('2d')
+const ctxLast5PlayOff = document.getElementById('last_5_playoff').getContext('2d')
 // eslint-disable-next-line no-unused-vars
-var last5PlayOffChart = new Chart(ctxLast5PlayOff, {
+const last5PlayOffChart = new Chart(ctxLast5PlayOff, {
   type: 'bar',
   data: makeData(DATA.last_5_world_cups.mostFrequentScoresPlayOffStage),
   options: {
@@ -104,14 +104,14 @@ var last5PlayOffChart = new Chart(ctxLast5PlayOff, {
   }
 })
 
-var ctxDiff = document.getElementById('diff').getContext('2d')
+const ctxDiff = document.getElementById('diff').getContext('2d')
 // eslint-disable-next-line no-unused-vars
-var diffChart = new Chart(ctxDiff, {
+const diffChart = new Chart(ctxDiff, {
   type: 'bar',
   data: makeData(DATA.last_5_world_cups.goalsDiff)
 })
 
-var scoresHeadings = ['Score', 'Number', 'Percent']
+const scoresHeadings = ['Score', 'Number', 'Percent']
 
 makeTable(document.querySelector('#scores-brazil'), scoresHeadings, DATA.brazil.mostFrequentScores)
 makeTable(document.querySelector('#scores-brazil-group'), scoresHeadings, DATA.brazil.mostFrequentScoresGroupStage)
@@ -133,7 +133,7 @@ makeTable(document.querySelector('#scores-france'), scoresHeadings, DATA.france.
 makeTable(document.querySelector('#scores-france-group'), scoresHeadings, DATA.france.mostFrequentScoresGroupStage)
 makeTable(document.querySelector('#scores-france-playoff'), scoresHeadings, DATA.france.mostFrequentScoresPlayOffStage)
 
-var diffHeadings = ['Goals Difference', 'Number', 'Percent']
+const diffHeadings = ['Goals Difference', 'Number', 'Percent']
 makeTable(document.querySelector('#diff-brazil'), diffHeadings, DATA.brazil.goalsDiff)
 makeTable(document.querySelector('#diff-south_africa'), diffHeadings, DATA.south_africa.goalsDiff)
 makeTable(document.querySelector('#diff-germany'), diffHeadings, DATA.germany.goalsDiff)
@@ -141,7 +141,7 @@ makeTable(document.querySelector('#diff-korea_japan'), diffHeadings, DATA.korea_
 makeTable(document.querySelector('#diff-france'), diffHeadings, DATA.france.goalsDiff)
 
 function makeData (arr) {
-  var out = {
+  const out = {
     labels: [],
     datasets: [{
       label: '',
