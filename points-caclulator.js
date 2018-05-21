586d76a3ec90eb63bc2e56ddbe88b9682e2067e0
Lukasz <skotniczny@outlook.com>
2018-05-21 22:59:15 +0200

    Add more tests to points-calculator

--- points-caclulator.js
+++ points-caclulator.js
@@ -14,6 +14,36 @@ const filter = (result) => {
 }
 
 console.log('Brazylia 2016')
+console.log('Typ 1:0:', calculatePoints('1-0', normalizeScores(BRAZIL)))
+console.log('Typ 2:1:', calculatePoints('2-1', normalizeScores(BRAZIL)))
+console.log()
+
+console.log('RPA 2010')
+console.log('Typ 1:0:', calculatePoints('1-0', normalizeScores(SOUTH_AFRICA)))
+console.log('Typ 2:1:', calculatePoints('2-1', normalizeScores(SOUTH_AFRICA)))
+console.log()
+
+console.log('Niemcy 2006')
+console.log('Typ 1:0:', calculatePoints('1-0', normalizeScores(GERMANY)))
+console.log('Typ 2:1:', calculatePoints('2-1', normalizeScores(GERMANY)))
+console.log()
+
+console.log('Korea/Japonia 2002')
+console.log('Typ 1:0:', calculatePoints('1-0', normalizeScores(KOREA_JAPAN)))
+console.log('Typ 2:1:', calculatePoints('2-1', normalizeScores(KOREA_JAPAN)))
+console.log()
+
+console.log('Francja 1988')
+console.log('Typ 1:0:', calculatePoints('1-0', normalizeScores(FRANCE)))
+console.log('Typ 2:1:', calculatePoints('2-1', normalizeScores(FRANCE)))
+console.log()
+
+console.log('Pięć ostatnich mistrzostw')
+console.log('Typ 1:0:', calculatePoints('1-0', normalizeScores(allResults)))
+console.log('Typ 2:1', calculatePoints('2-1', normalizeScores(allResults)))
+
+console.log('Tylko wyniki inne niż 1-0 i 2-1')
+console.log('Brazylia 2016')
 console.log('Typ 1:0:', calculatePoints('1-0', normalizeScores(BRAZIL).filter(filter)))
 console.log('Typ 2:1:', calculatePoints('2-1', normalizeScores(BRAZIL).filter(filter)))
 console.log()
@@ -41,3 +71,12 @@ console.log()
 console.log('Pięć ostatnich mistrzostw')
 console.log('Typ 1:0:', calculatePoints('1-0', normalizeScores(allResults).filter(filter)))
 console.log('Typ 2:1', calculatePoints('2-1', normalizeScores(allResults).filter(filter)))
+
+console.log('Test')
+console.log('Typ 0:0', calculatePoints('0-0', ['1-2']))
+console.log('Typ 1:0', calculatePoints('1-0', ['0-1']))
+console.log('Typ 1:0', calculatePoints('1-0', ['4-2']))
+console.log('Typ 1:1', calculatePoints('1-1', ['2-2']))
+console.log('Typ 3:1', calculatePoints('3-1', ['4-2']))
+console.log('Typ 2:1', calculatePoints('2-1', ['4-1']))
+console.log('Typ 3:1', calculatePoints('3-1', ['3-1']))
